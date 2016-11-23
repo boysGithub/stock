bf96af2af44ee467f39a00cc7408398cf88deb73
duke <2567134961@qq.com>
2016-11-23 13:39:43 +0800

    duke
    修复dev版本同步提交

--- application/order/validate/Share.php
+++ application/order/validate/Share.php
@@ -11,6 +11,8 @@ class Share extends Validate
 	protected $rule = [
 		['uid','require|number','用户id不能为空|必须为数字'],
 		['stock','require|number|length:6','股票代码不能为空|请输入正确的股票代码|股票代码必须是6位数字'],
+		['stime','require|date|dateFormat:Y-m-d','开始时间不能为空|请输入有效的日期时间|日期的格式不对,采用Y-m-d'],
+		['etime','require|date|dateFormat:Y-m-d','结束时间不能为空|请输入有效的日期时间|日期的格式不对,采用Y-m-d'],
 	];
 }
 ?>
