a868b193a08e60afd4ea631b593690ffd5177730
zhangpeng <910220226@qq.com>
2016-11-25 16:07:56 +0800

    交易页面，match接口修改

--- application/user/validate/Match.php
+++ application/user/validate/Match.php
@@ -9,13 +9,17 @@ class Match extends Validate
 {
 	protected $rule = [
 		['id','require|number','比赛id不能为空|必须为数字'],
-		['match_id','require|number','比赛id不能为空|必须为数字'],
 		['uid','require|number','用户id不能为空|必须为数字'],
+		['name','require','比赛名称不能为空'],
+		['start_date','require|date','开始日期不能为空|必须为日期'],
+		['end_date','require|date','结束日期不能为空|必须为日期'],
+		['initial_capital','require|number','初始资金不能为空|必须为数字'],
 	];
 
 	protected $scene = [
         'match'  =>  ['id'],
-        'detail'  =>  ['match_id','uid'],
+        'detail'  =>  ['match_id','id'],
+        'add' => ['name','start_date','end_date','initial_capital']
     ];
 }
 ?>
