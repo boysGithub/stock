8339bf0af91b9c64e1b52863d20e2d3b01d3c35b
duke <2567134961@qq.com>
2016-12-04 17:14:26 +0800

    duke
    增加访问安全验证

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -4,7 +4,9 @@ namespace app\index\controller;
 
 use think\Controller;
 use think\Request;
-
+use think\Config;
+use think\Db;
+use think\cache\driver\Redis;
 /**
 * 
 */
@@ -12,9 +14,36 @@ class Base extends Controller
 {
 	public $_limit = 20; //显示的条数
 	public $_stockFunds = 1000000; //股票账户初始金额
-	public $_scale = 0.0003; //股票手续
-    // public function __construct()
-    // {
-        
-    // }
+	public $_scale = 0.0003; //股票手续费
+    public function _initialize()
+    {
+    	$data = input('get.');
+
+    	$res = $this->validate($data,'TellToken');
+        if (true !== $res) {
+            exit(JN(['status'=>'failed','data'=>$res]));
+        }
+        $redis = new Redis;
+        //固定的token
+    	$token = Config::has("stocktell.token") ? Config::get('stocktell.token') : '';
+    	//随机token字符串
+        if($redis->has('rand_token')){
+        	$randToken = $redis->get('rand_token');
+        }else{
+        	$randToken = getRandChar(10);
+        	Db::startTrans();
+        	try {
+        		$sql = "UPDATE `ts_user` a,(select uid from `ts_user`) obj set a.stock_token=sha1(concat('{$token}',obj.uid,'{$randToken}')) where a.uid=obj.uid";
+        		Db::connect('sjq1')->query($sql);
+        		Db::commit();
+        		$redis->set('rand_token',$randToken,30);
+        	} catch (\Exception $e) {
+        		Db::rollback();
+        		exit(JN(['status'=>'failed','data'=>'token生成失败']));
+        	}
+        }
+    	if(sha1($token.$data['uid'].$randToken) != $data['token']){
+    		exit(JN(['status'=>'failed','data'=>'token过期，请重新登录']));
+    	}
+    }
 }
