3cefd6abe2c6757e2c2bdbf9a7a26990d0f67491
duke <2567134961@qq.com>
2016-12-12 18:57:19 +0800

    duke
    提交

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -18,7 +18,7 @@ class Base extends Controller
 {
 	public $_limit = 20; //显示的条数
 	public $_stockFunds = 1000000; //股票账户初始金额
-	public $_scale = 0.0003; //股票手续费
+	public $_scale = 0.001; //股票手续费
     public $_sorts = 1;
     /**
      * [createStock 股票账户的创建]
