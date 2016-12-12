25c3cd0fa49470b97f37344e01d8ac0f75b6eeed
duke <2567134961@qq.com>
2016-12-12 16:51:54 +0800

    duke
    修改新建的用户不能创建账户问题

--- application/user/controller/Index.php
+++ application/user/controller/Index.php
@@ -181,6 +181,7 @@ class Index extends Base
      */
     public function read($id)
     {   
+        $this->_base->checkToken();
         $stockFunds = $this->_base->_stockFunds;
         $fund = UserFunds::where(['uid'=>$id])->Field('id,uid,funds,time,operationTime,available_funds,sorts,total_rate,avg_position_day,total_profit_rank,week_avg_profit_rate,win_rate,success_rate')->find();
         //获取用户资产信息
